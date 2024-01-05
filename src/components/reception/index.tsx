5c8ddf1eefdebd0a0d0ad6c44ea78d91296fd93c
entornoInteractivo <cristianvelasco1529@gmail.com>
2024-01-04 23:03:22 -0500

    detail reception

--- src/components/reception/index.tsx
+++ src/components/reception/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import treeSvg from '../../assets/principal-tree-wedding-bg0.svg'
 import Information from '../information';
+import styles from './reception.module.css';
 
 type infoWedding = {
   city: string,
@@ -18,27 +19,31 @@ export default function ReceptionAndWedding({
   receptionTime
 }: infoWedding) {
   return (
-    <section>
-      <h2>{city}</h2>
-      
-      <Information
-        title={weddingTime}
-        paragraph1={weddingPlace}
-        paragraph2={'Ceremonia'}
-      />
+    <section className={styles.reception}>
+      <span className={styles['reception-title']}>Lugar</span>
 
-      <Information
-        title={receptionTime}
-        paragraph1={receptionPlace}
-        paragraph2={'Recepción'}
-      />
+      <div className={styles['reception-wrapper']}>
+        <h2>{city}</h2>
+        
+        <Information
+          title={weddingTime}
+          paragraph1={weddingPlace}
+          paragraph2={'Ceremonia'}
+        />
 
-      <Image
-        src={treeSvg}
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
+        <Information
+          title={receptionTime}
+          paragraph1={receptionPlace}
+          paragraph2={'Recepción'}
+        />
+
+        <Image
+          src={treeSvg}
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+      </div>
     </section>
   )
 }
