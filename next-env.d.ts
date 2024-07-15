117e10ba3297c5b4f8d15455141a7f7422e5468c
crisdeve <crisdevels.co@gmail.com>
2024-07-14 19:01:06 -0500

    redeclare mp3 modules

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,10 +2,7 @@
 /// <reference types="next/image-types/global" />
 /// <reference types="next/image-types/global" />
 
-declare module '*.mp3' {
-  const content: any;
-  export default content;
-}
+declare module '*.mp3';
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
